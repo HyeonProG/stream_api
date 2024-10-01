d88f20c2d2a3ad6af6938b015df831fb8e2a7d58
HyeonProG <guswo3443@naver.com>
2024-10-01 12:05:21 +0900

    Stream API

---
+++ src/challenge/Challenge2.java
@@ -0,0 +1,18 @@
+package challenge;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Challenge2 {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("apple", "banana", "cherry", "date");
+
+        // 2. 문자열 길이를 계산하여 새로운 리스트로 반환하는 코드를 작성하세요.
+        // 힌트: map()을 사용합니다.
+        List<Integer> lengths = words.stream()
+                                .map(str -> str.length())
+                                .collect(Collectors.toList());
+        System.out.println(lengths);
+    }
+}
